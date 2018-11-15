df783519219b94a8522f2d73fd1b3d0c609291de
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-11-15 20:25:29 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -12,22 +12,29 @@ class Application
         resp.write "#{item}\n"
       end
     elsif req.path.match(/cart/)
-
       if @@cart.empty?
         resp.write "Your cart is empty"
       else
         @@cart.each do |item|
           resp.write "#{item}\n"
         end
-      end 
+      end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+
+    elsif req.path.match(/add/)
+
+
     else
       resp.write "Path Not Found"
     end
 
     resp.finish
+
+
+
+
   end
 
 
