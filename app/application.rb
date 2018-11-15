57a8354412c1fcef185664a48954cbecab8edbda
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-11-15 20:27:33 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -28,8 +28,7 @@ class Application
         @@cart << req.param["item"]
       else
         resp.write "We don't have that item"
-
-
+      end 
     else
       resp.write "Path Not Found"
     end
